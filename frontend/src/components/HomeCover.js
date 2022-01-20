936b383d707c348d90d469c88ed7bff626b3d452
Rayato159 <rayato159@gmail.com>
2022-01-21 01:22:35 +0700

    :sparkles: cover image logo added

--- frontend/src/components/HomeCover.js
+++ frontend/src/components/HomeCover.js
@@ -5,20 +5,22 @@ export const HomeCover = () => {
       <div className='relative w-full top-0'>
           <div className="bg-mycolor-600">
             <div className="max-w-6xl mx-auto">
-                <div className="flex flex-col p-4">
-                    <div className='font-extrabold font-serif italic md:text-7xl text-4xl text-white'>
-                        Mongsue
+                <div className="flex justify-between items-center">
+                    <div className="flex flex-col py-10 px-4">
+                        <div className='font-extrabold font-serif italic md:text-6xl text-4xl text-white'>
+                            Mongsue
+                        </div>
+                        <div className='text-sm md:text-xl text-white py-3'>
+                            มองสือ ที่แปลว่ามือสอง ซื้อ-ขาย ของมือสองเพียงเอื้อมมือ
+                        </div>
+                        <div className='py-2'>
+                            <button className='bg-mycolor-200 hover:bg-mycolor-100 text-xl px-10 py-1'>
+                                ลงขายเลย
+                            </button>
+                        </div>
                     </div>
-                    <div className='text-sm md:text-2xl text-white py-3'>
-                        มองสือ ที่แปลว่ามือสอง ซื้อ-ขาย ของมือสองเพียงเอื้อมมือ
-                    </div>
-                    <div className='py-3'>
-                        <button className='bg-mycolor-200 hover:bg-mycolor-100 text-xl px-10 py-1'>
-                            ลงขายเลย
-                        </button>
-                    </div>
-                    <div className='hidden md:flex justify-end'>
-                        <img className='h-40 w-auto' src="./assets/coverImg/cessnaCitationX.png" alt="cover-img" />
+                    <div>
+                        <img className='h-auto w-56' src="./assets/logo/rayato.png" alt="logo" />
                     </div>
                 </div>
             </div>
