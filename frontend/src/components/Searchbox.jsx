218520347761ca41df3d446340002a3684aa5cc6
Rayato159 <rayato159@gmail.com>
2022-01-23 01:08:26 +0700

    :sparkels: trying with Vite

--- frontend/src/components/Searchbox.jsx
+++ frontend/src/components/Searchbox.jsx
@@ -4,7 +4,7 @@ export const Searchbox = ({ onSumbitHandle, onChangeHandle }) => {
   return (
     <form onSubmit={(e) => onSumbitHandle(e)} className='flex justify-center'>
         <input onChange={(e) => onChangeHandle(e.target.value)} className='px-2 py-1 w-72 focus:outline-none' type="text" placeholder='กำลังมองหาอะไรอยู่...'/>
-        <button type="submit" className='bg-mycolor-400 hover:bg-mycolor-500' type='submit'>
+        <button type="submit" className='bg-mycolor-400 hover:bg-mycolor-500'>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
