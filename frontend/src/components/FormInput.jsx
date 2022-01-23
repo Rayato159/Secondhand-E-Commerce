025f97b268fac7d3263600844c6ccb4ae40e62dc
Rayato159 <rayato159@gmail.com>
2022-01-23 17:48:37 +0700

    :sparkles: show and hide password are added.

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -8,7 +8,7 @@ export const FormInput = ({ label, placeholder, event }) => {
     return (
         <div className='flex flex-col space-y-1'>
             <FormLabel message={label}/>
-            <input onChange={(e) => event(e.target.value)} className='w-full border-b-2 border-mycolor-600 bg-mycolor-200 md:py-1 px-2 focus:outline-none focus:bg-neutral-100 duration-300 text-sm' type='text' placeholder={placeholder} />
+            <input onChange={(e) => event(e.target.value)} className='w-full h-8 border-b-2 border-mycolor-600 bg-neutral-200 md:py-1 px-2 focus:outline-none duration-300 text-sm' type='text' placeholder={placeholder} />
         </div>
     )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -50,13 +50,24 @@ export const Register = () => {
                             {/* Password */}
                             <div className='flex flex-col space-y-1'>
                                 <FormLabel message={'Password'}/>
-                                <input onChange={(e) => setPassword(e.target.value)} className='w-full border-b-2 border-mycolor-600 bg-mycolor-200 md:py-1 px-2 focus:outline-none focus:bg-neutral-100 duration-300 text-sm' type="password" />
+                                <div className="flex items-center">
+                                    <input onChange={(e) => setPassword(e.target.value)} className='w-full h-8 border-b-2 border-mycolor-600 bg-neutral-200 md:py-1 px-2 focus:outline-none duration-300 text-sm' type={isShowPassword? "text":"password"} />
+                                    {isShowPassword?
+                                        <svg onClick={() => setIsShowPassword(!isShowPassword)} xmlns="http://www.w3.org/2000/svg" className="h-8 w-auto p-1 bg-mycolor-600 text-white cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                        </svg>:
+                                        <svg onClick={() => setIsShowPassword(!isShowPassword)} xmlns="http://www.w3.org/2000/svg" className="h-8 w-auto p-1 bg-mycolor-600 text-white cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+                                        </svg>
+                                    }
+                                </div>
                             </div>
 
                             {/* Confirm-Password */}
                             <div className='flex flex-col space-y-1'>
                                 <FormLabel message={'Password-Confirm'}/>
-                                <input onChange={(e) => setPasswordConfirm(e.target.value)} className='w-full border-b-2 border-mycolor-600 bg-mycolor-200 md:py-1 px-2 focus:outline-none focus:bg-neutral-100 duration-300 text-sm' type="password" />
+                                <input onChange={(e) => setPasswordConfirm(e.target.value)} className='w-full h-8 border-b-2 border-mycolor-600 bg-neutral-200 md:py-1 px-2 focus:outline-none duration-300 text-sm' type={isShowPassword? "text":"password"} />
                             </div>
 
 
