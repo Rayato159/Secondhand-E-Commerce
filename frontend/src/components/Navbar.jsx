25d9246fa688b6eea059a5cad00897268d3c9018
Rayato159 <rayato159@gmail.com>
2022-01-25 00:33:44 +0700

    :sparkles: navbar fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -57,88 +57,86 @@ export const Navbar = () => {
   }, [isAuth])
 
   return (
-      <div>
-        <nav className='bg-mycolor-200 sticky top-0 w-full z-50 shadow-lg'>
-          <div className='max-w-7xl mx-auto'>
-            <div className='flex justify-between'>
-              {/* Left */}
-              <div className='flex space-x-6 items-center p-4'>
+    <nav className='bg-mycolor-200 sticky top-0 w-full z-50 shadow-lg'>
+      <div className='max-w-7xl mx-auto'>
+        <div className='flex justify-between'>
+          {/* Left */}
+          <div className='flex space-x-6 items-center p-4'>
 
-                {/* Toggle Button */}
-                <div className='md:hidden flex'>
-                  <button onClick={() => setIsShowToggle(!isShowToggle)} className='items-center'>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                  </button>
-                </div>
+            {/* Toggle Button */}
+            <div className='md:hidden flex'>
+              <button onClick={() => setIsShowToggle(!isShowToggle)} className='items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              </button>
+            </div>
 
-                {/* Logo */}
-                <Link to="/" className='font-extrabold italic text-2xl'>
-                  Mongsue
-                </Link>
+            {/* Logo */}
+            <Link to="/" className='font-extrabold italic text-2xl'>
+              Mongsue
+            </Link>
 
-                {/* AboutUs */}
-                <div className='hidden md:flex'>
-                  <Link to="about" className='text-sm hover:text-gray-500'>
-                    เกี่ยวกับเรา
-                  </Link>
-                </div>
-              </div>
+            {/* AboutUs */}
+            <div className='hidden md:flex'>
+              <Link to="about" className='text-sm hover:text-gray-500'>
+                เกี่ยวกับเรา
+              </Link>
+            </div>
+          </div>
 
-              {/* Mid Searchbox */}
-              <div className='hidden md:flex items-center p-4'>
+          {/* Mid Searchbox */}
+          <div className='hidden md:flex items-center p-4'>
+            <Searchbox onSumbitHandle={(e) => parentOnSubmit(e)} onChangeHandle={(value) => setSearchEvent(value)} />
+          </div>
+
+          {/* Right */}
+          {isUser?
+            <div className='hidden md:flex space-x-6 items-center p-4'>
+              <LinkNavbar path={"profile"} message={isUser.first_name}/>
+              <button onClick={() => dispatch(logout())} className='text-sm hover:text-gray-500'>
+                ออกจากระบบ
+              </button>
+            </div>:
+            <div className='hidden md:flex space-x-6 items-center p-4'>
+              <LinkNavbar path={"login"} message={"เข้าสู่ระบบ"}/>
+              <LinkNavbar path={"register"} message={"สมัครสมาชิก"}/>
+            </div>
+          }
+        </div>
+
+        {/* Toggle Bar */}
+        {isShowToggle?
+          <div className='md:hidden w-full'>
+            <div className="flex flex-col">
+
+              {/* Searchbox */}
+              <div className='py-3'>
                 <Searchbox onSumbitHandle={(e) => parentOnSubmit(e)} onChangeHandle={(value) => setSearchEvent(value)} />
               </div>
 
-              {/* Right */}
+              {/* Login */}
               {isUser?
-                <div className='hidden md:flex space-x-6 items-center p-4'>
-                  <LinkNavbar path={"profile"} message={isUser.first_name}/>
-                  <button onClick={() => dispatch(logout())} className='text-sm hover:text-gray-500'>
-                    ออกจากระบบ
+                <ResLinkNavbar path={"/profile"} message={isUser.first_name}/>:<ResLinkNavbar path={"/login"} message={"เข้าสู่ระบบ"}/>
+              }
+
+              {/* Register */}
+              {isUser?
+                <div>
+                  <button onClick={() => dispatch(logout())} className='block w-full text-sm text-center hover:bg-mycolor-100 p-4'>
+                      Logout
                   </button>
                 </div>:
-                <div className='hidden md:flex space-x-6 items-center p-4'>
-                  <LinkNavbar path={"login"} message={"เข้าสู่ระบบ"}/>
-                  <LinkNavbar path={"register"} message={"สมัครสมาชิก"}/>
-                </div>
+                <ResLinkNavbar path={"register"} message={"สมัครสมาชิก"}/>
               }
+
+              {/* AboutUs */}
+              <ResLinkNavbar path={"about"} message={"เกี่ยวกับเรา"}/>
             </div>
-
-            {/* Toggle Bar */}
-            {isShowToggle?
-              <div className='md:hidden w-full'>
-                <div className="flex flex-col">
-
-                  {/* Searchbox */}
-                  <div className='py-3'>
-                    <Searchbox onSumbitHandle={(e) => parentOnSubmit(e)} onChangeHandle={(value) => setSearchEvent(value)} />
-                  </div>
-
-                  {/* Login */}
-                  {isUser?
-                    <ResLinkNavbar path={"/profile"} message={isUser.first_name}/>:<ResLinkNavbar path={"/login"} message={"เข้าสู่ระบบ"}/>
-                  }
-
-                  {/* Register */}
-                  {isUser?
-                    <div>
-                      <button onClick={() => dispatch(logout())} className='block w-full text-sm text-center hover:bg-mycolor-100 p-4'>
-                          Logout
-                      </button>
-                    </div>:
-                    <ResLinkNavbar path={"register"} message={"สมัครสมาชิก"}/>
-                  }
-
-                  {/* AboutUs */}
-                  <ResLinkNavbar path={"about"} message={"เกี่ยวกับเรา"}/>
-                </div>
-              </div>
-            :null}
-
           </div>
-        </nav>
+        :null}
+
       </div>
+    </nav>
   )
 }
