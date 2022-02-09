d18edf653fc5853996794a90714ed20762450404
Rayato159 <rayato159@gmail.com>
2022-02-10 01:13:20 +0700

    :sparkles: react-hook-form added.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -53,91 +53,91 @@ export const Register = () => {
     }, [isToken])
     
     return (
-    <div className="w-full py-10">
-        <div className="max-w-md mx-auto shadow-xl bg-white px-4 py-10 rounded-xl">
-            <div className="flex justify-center">
-                {/* Register forms */}
-                <form onSubmit={handleSubmit(onSubmit)} className="w-72 flex flex-col space-y-6">
+        <div className="w-full py-10">
+            <div className="max-w-md mx-auto shadow-xl bg-white px-4 py-10 rounded-xl">
+                <div className="flex justify-center">
+                    {/* Register forms */}
+                    <form onSubmit={handleSubmit(onSubmit)} className="w-72 flex flex-col space-y-6">
 
-                    {/* Header */}
-                    <div className='flex space-x-2 items-center'>
-                        <div>
-                            <AiOutlineUserAdd className='h-7 w-7' />
-                        </div>
-                        <div className='text-2xl font-bold'>
-                            สมัครสมาชิก
-                        </div>
-                    </div>
-
-                    {/* Form */}
-                    <div className='w-full flex flex-col space-y-4'>
-                        {/* First Name */}
+                        {/* Header */}
                         <div className='flex space-x-2 items-center'>
-                            <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" placeholder="First Name"
-                                {...register("first_name"),  { required: true }}
-                            />
-                        </div>
-
-                        {/* Last Name */}
-                        <div className='flex space-x-2 items-center'>
-                            <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" placeholder="Last Name"
-                                {...register("last_name"),  { required: true }}
-                            />
-                        </div>
-
-                        {/* Phone Number */}
-                        <div className='flex space-x-2 items-center'>
-                            <AiOutlinePhone className='w-6 h-6' />
-                            <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" placeholder="Phone Number"
-                                {...register("phone_number"),  { required: true, pattern: /^[0-9]{0,10}$/ }}
-                            />
-                        </div>
-                        
-                        {/* Email */}
-                        <div className='flex space-x-2 items-center'>
-                            <AiOutlineMail className='w-6 h-6' />
-                            <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" placeholder="Email"
-                                {...register("email"),  { required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ }}
-                            />
-                        </div>
-
-                        {/* Password */}
-                        <div className='relative flex space-x-2 items-center justify-end'>
-                            <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type={isShowPassword? "text": "password"} maxLength={36} placeholder="Password"
-                                {...register("password"),  { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/ }}
-                            />
-                            {isShowPassword?
-                                <AiOutlineEye onClick={() => setIsShowPassword(!isShowPassword)} className='absolute w-6 h-6 cursor-pointer' />
-                                :<AiOutlineEyeInvisible onClick={() => setIsShowPassword(!isShowPassword)} className='absolute w-6 h-6 cursor-pointer' />
-                            }
-                        </div>
-
-                        {/* Password-Confirm */}
-                        <div className='flex space-x-2 items-center'>
-                            <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type={isShowPassword? "text": "password"} maxLength={36} placeholder="Password-Confirm"
-                                {...register("password_confirm"),  { required: true }}
-                            />
-                        </div>
-
-                        {/* Address */}
-                        <div className='flex space-x-2 items-center'>
-                            <textarea className="w-full bg-gray-100 border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" rows="3" placeholder="Address"
-                                {...register("address"),  { required: true }}
-                            />
-                        </div>
-                    </div>
-                    
-                    {/* Button */}
-                    <div className='pt-6'>
-                        <button className="bg-mycolor-300 p-2 rounded-full shadow-md w-full hover:bg-mycolor-200 hover:-translate-y-1 duration-300" type="submit">
-                            <div className="font-bold">
+                            <div>
+                                <AiOutlineUserAdd className='h-7 w-7' />
+                            </div>
+                            <div className='text-2xl font-bold'>
                                 สมัครสมาชิก
                             </div>
-                        </button>
-                    </div>
-                </form>
+                        </div>
+
+                        {/* Form */}
+                        <div className='w-full flex flex-col space-y-4'>
+                            {/* First Name */}
+                            <div className='flex space-x-2 items-center'>
+                                <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" placeholder="First Name"
+                                    {...register("first_name"),  { required: true }}
+                                />
+                            </div>
+
+                            {/* Last Name */}
+                            <div className='flex space-x-2 items-center'>
+                                <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" placeholder="Last Name"
+                                    {...register("last_name"),  { required: true }}
+                                />
+                            </div>
+
+                            {/* Phone Number */}
+                            <div className='flex space-x-2 items-center'>
+                                <AiOutlinePhone className='w-6 h-6' />
+                                <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" placeholder="Phone Number"
+                                    {...register("phone_number"),  { required: true, pattern: /^[0-9]{0,10}$/ }}
+                                />
+                            </div>
+                            
+                            {/* Email */}
+                            <div className='flex space-x-2 items-center'>
+                                <AiOutlineMail className='w-6 h-6' />
+                                <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" placeholder="Email"
+                                    {...register("email"),  { required: true, pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ }}
+                                />
+                            </div>
+
+                            {/* Password */}
+                            <div className='relative flex space-x-2 items-center justify-end'>
+                                <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type={isShowPassword? "text": "password"} maxLength={36} placeholder="Password"
+                                    {...register("password"),  { required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/ }}
+                                />
+                                {isShowPassword?
+                                    <AiOutlineEye onClick={() => setIsShowPassword(!isShowPassword)} className='absolute w-6 h-6 cursor-pointer' />
+                                    :<AiOutlineEyeInvisible onClick={() => setIsShowPassword(!isShowPassword)} className='absolute w-6 h-6 cursor-pointer' />
+                                }
+                            </div>
+
+                            {/* Password-Confirm */}
+                            <div className='flex space-x-2 items-center'>
+                                <input className="w-full border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type={isShowPassword? "text": "password"} maxLength={36} placeholder="Password-Confirm"
+                                    {...register("password_confirm"),  { required: true }}
+                                />
+                            </div>
+
+                            {/* Address */}
+                            <div className='flex space-x-2 items-center'>
+                                <textarea className="w-full bg-gray-100 border-b-2 border-mycolor-500 py-1 px-2 focus:outline-none focus:border-black" type="text" rows="3" placeholder="Address"
+                                    {...register("address"),  { required: true }}
+                                />
+                            </div>
+                        </div>
+                        
+                        {/* Button */}
+                        <div className='pt-6'>
+                            <button className="bg-mycolor-300 p-2 rounded-full shadow-md w-full hover:bg-mycolor-200 hover:-translate-y-1 duration-300" type="submit">
+                                <div className="font-bold">
+                                    สมัครสมาชิก
+                                </div>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
