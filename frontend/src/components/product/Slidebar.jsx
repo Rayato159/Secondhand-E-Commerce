bd4d9ac978c51912b13aa5089f0147cb2865a7d7
Rayato159 <rayato159@gmail.com>
2022-02-19 04:57:28 +0700

    :sparkles: images slide completed

--- frontend/src/components/product/Slidebar.jsx
+++ frontend/src/components/product/Slidebar.jsx
@@ -9,6 +9,9 @@ export const Slidebar = ({ props }) => {
 
     // Hooks State
     const [currentIndex, setCurrentIndex] = useState(0)
+    const [currentPhoto, setCurrentPhoto] = useState(props[0].product_photo_id)
+    const [lastIndex, setLastIndex] = useState(currentIndex % props.length - 1)
+    const [images, setImages] = useState(props.slice(0, props.length))
     const slideRef = useRef()
 
     const removeAnimation = () => {
@@ -23,18 +26,28 @@ export const Slidebar = ({ props }) => {
     const onNextClick = () => {
         count = (count + 1) % props.length
         setCurrentIndex(count)
+        setCurrentPhoto(props[count].product_photo_id)
+
+        // Queue images forward
+        images.push(images.shift())
+
         slideRef.current.classList.add('fade-anim')
     }
 
     const onPrevClick = () => {
         const productsLength = props.length
-        count = (currentIndex + productsLength - 1) % productsLength;
+        count = (currentIndex + productsLength - 1) % productsLength
         setCurrentIndex(count)
+        setCurrentPhoto(props[count].product_photo_id)
+
+        // Queue images backward
+        images.splice(0, 0, images.pop())
+
         slideRef.current.classList.add('fade-anim')
     }
 
     return (
-        <div className='flex flex-col space-y-4'>
+        <div className='flex flex-col space-y-4 items-start'>
             <div ref={slideRef} className='flex items-center h-full w-full select-none relative'>
                 <div className='flex flex-col space-y-3 aspect-video'>
                     <img className='md:w-96 md:h-64 w-96 h-56 overflow-hidden object-cover' src={props[currentIndex]? props[currentIndex].path: ''} />
@@ -50,14 +63,14 @@ export const Slidebar = ({ props }) => {
             </div>
             <div className='flex space-x-3'>
                 {props &&
-                    props.map((image, i) => {
-                        if(i === currentIndex) {
+                    images.slice(0, lastIndex).map((image) => {
+                        if(image.product_photo_id === currentPhoto) {
                             return (
-                                <img key={image.product_photo_id} className='h-12 w-20 object-cover border-2 border-black shadow-md' src={image? image.path: ''} />
+                                <img key={image.product_photo_id} className='aspect-video h-12 w-20 object-cover border-2 border-black shadow-md' src={image? image.path: ''} />
                             )
                         } else {
                             return (
-                                <img onClick={() => setCurrentIndex(i)} key={image.product_photo_id} className='h-12 w-20 object-cover shadow-md hover:cursor-pointer' src={image? image.path: ''} />
+                                <img onClick={() => setCurrentIndex(i)} key={image.product_photo_id} className='aspect-video h-12 w-20 object-cover shadow-md hover:cursor-pointer' src={image? image.path: ''} />
                             )
                         }
                     })
